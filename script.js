fa77fd1e12431d21fb378536091e971ee19d7b44
arshad-hash <142026560+arshad-hash@users.noreply.github.com>
2023-11-15 23:37:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-//your JS code here. If required.
+//your JS code here. If requi
+function mapLetters(str){
+    let object ={
+        
+    };
+    for (let i = 0; i < str.length; i++) {
+        if(object[str[i]]===undefined)
+        {
+            object[str[i]]=[i]; 
+        }
+        else{
+            object[str[i]].push(i);
+        }
+        
+    } console.log(object);
+ return object ;
+};
+
+    let str=prompt("createObject:");
+    alert(mapLetters(str));
